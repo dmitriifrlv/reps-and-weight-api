8946d42b2b9eceaa8ae0d09b8590909fa6d21651
Dmitrii Frolov <65853205+dmitriifrlv@users.noreply.github.com>
2021-05-28 22:26:27 +0500

    initial commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 const dbURI = process.env.dbURI
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then((result)=>{ 
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
     console.log('connected to db')
   })
   .catch((err)=>console.log(err))
