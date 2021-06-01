041531f71b53f55e49da04260c085e73a7a2be8f
Dmitrii Frolov <65853205+dmitriifrlv@users.noreply.github.com>
2021-06-01 20:07:25 +0500

    changed exercise model

--- models/user.js
+++ models/user.js
@@ -3,18 +3,7 @@ const { Schema } = mongoose;
 
 const exerciseSchema = new Schema({
   exercise: String,
-  reps1: Number,
-  reps2: Number,
-  reps3: Number,
-  reps4: Number,
-  reps5: Number,
-  reps6: Number,
-  weight1: Number,
-  weight2: Number,
-  weight3: Number,
-  weight4: Number,
-  weight5: Number,
-  weight6: Number,
+  sets: [],
   measure: String
 })
 
